c0e85db92711219ebc15d555a4f2e196894f307f
Rohit Kumar <rohitkumar.rk0600@gmail.com>
2025-03-09 23:18:22 +0530

    Added backpropogation alogrithm for NN

--- helper_functions.py
+++ helper_functions.py
@@ -1,8 +1,37 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def softmax(x):
-    exps = np.exp(x - np.max(x, axis=1, keepdims=True))
-    return exps / np.sum(exps, axis=1, keepdims=True)
+class ActivationFunctions:
+    @staticmethod
+    def sigmoid(x , derivative=False):
+        '''
+        Sigmoid activation function
+        '''
+        if derivative:
+            return x * (1 - x)
+        return 1 / (1 + np.exp(-x))
+    
+    @staticmethod
+    def relu(x, derivative=False):
+        '''
+        ReLU activation function
+        '''
+        if derivative:
+            return np.where(x <= 0, 0, 1)
+        
+        return np.maximum(0, x)
+    
+    @staticmethod
+    def tanh(x):
+        '''
+        Tanh activation function
+        '''
+        return np.tanh(x)
+    
+    @staticmethod
+    def softmax(x):
+        '''
+        Softmax activation function
+        '''
+        assert x.ndim == 2, "Input tensor should be a 2D tensor"
+        exps = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exps / np.sum(exps, axis=1, keepdims=True)
--- neural_network.py
+++ neural_network.py
@@ -16,12 +16,17 @@ class NeuralNetwork():
         self.activation_function = activation_function
         self.output_activation = output_activation
 
+        self.forward_pass = []
+        self.backward_pass = []
+
+
         self.weights = None
         self.biases = None
 
-        self.__initialize_weights_and_biases()
+        self.__random_initialize_weights_and_biases()
 
-    def __initialize_weights_and_biases(self): 
+
+    def __random_initialize_weights_and_biases(self): 
         self.weights = [np.random.randn(self.input_size, self.neurons_per_layer[0])]
         self.biases = [np.random.randn(self.neurons_per_layer[0])]
 
@@ -31,23 +36,46 @@ class NeuralNetwork():
 
         self.weights.append(np.random.randn(self.neurons_per_layer[-1], self.output_size))
         self.biases.append(np.random.randn(self.output_size))
-
+ 
     def forward(self, X):
+        self.flag = True
         assert X.shape[1] == self.input_size, f"Input size is not matching with the input size of the network. Expected {self.input_size} but got {X.shape[1]}"
         f_x = X@self.weights[0] + self.biases[0]
         h_x = self.activation_function(f_x)
 
+        self.forward_pass.append((h_x.copy(), f_x.copy()))
+
         for i in range(1, self.hidden_layers):
             f_x = h_x @ self.weights[i] + self.biases[i]
             h_x = self.activation_function(f_x)
+            self.forward_pass.append((h_x.copy(), f_x.copy()))
 
         f_x = h_x @ self.weights[-1] + self.biases[-1]
         y = self.output_activation(f_x)
+        self.forward_pass.append((y.copy(), f_x.copy()))
 
         return y
 
-    def backward(self, x, y):
-        pass 
+    def backward(self, X, y):
+        assert self.flag == True, "Forward pass should be called before calling backward pass"
+        
+        assert type(y) == np.ndarray, "Expected numpy array as input"
+        assert y.ndim == 2, "Expected 2D numpy array as input"
+        assert y.shape[1] == self.output_size, f"Output size is not matching with the output size of the network. Expected {self.output_size} but got {y.shape[1]}"
 
+        gradient_wrt_pre_activation = -(y - self.forward_pass[-1][0]) # gradient of cross entropy loss
 
-         
+        for i in range(self.hidden_layers, 0, -1):
+            gradient_wrt_weights = gradient_wrt_pre_activation[:, :, np.newaxis] @ self.forward_pass[i-1][0][:, np.newaxis, :]
+            gradient_wrt_biases = gradient_wrt_pre_activation
+            gradient_wrt_activation = gradient_wrt_pre_activation @ self.weights[i].T
+            gradient_wrt_pre_activation = gradient_wrt_activation * self.activation_function(self.forward_pass[i-1][-1], derivative=True)
+            self.backward_pass.append((gradient_wrt_weights, gradient_wrt_biases))
+
+        gradient_wrt_weights = gradient_wrt_pre_activation[:, :, np.newaxis] @ X[:, np.newaxis, :]
+        gradient_wrt_biases = gradient_wrt_pre_activation
+
+        self.backward_pass.append((gradient_wrt_weights, gradient_wrt_biases))
+
+        return self.backward_pass
+
